b864a2aedf932ca65b6ef33ad939687818632ab5
nhillson <116381076+nhillson@users.noreply.github.com>
2023-06-01 15:06:22 -0500

    Update smarties.py
    removed dict initialization
    initialized variables within loop
    changed looping method for dict while accumulating time

--- smarties.py
+++ smarties.py
@@ -1,24 +1,19 @@
 # https://dmoj.ca/problem/ecoo15r1p1
-# Probably should have initialized inside loop so I wouldn't have to reset variables each iteration
-smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
-time = 0
 # 10 test cases
 for _ in range(10):
-    while True:
+    smarties = {}
+    time = 0
+    while color != "end of box":
         # Take input and increment value in smarties dict
         color = input()
-        if color == "end of box":
-            break
         smarties[color] += 1
     # Loop through smarties dict accumulating time
-    for smartie in smarties:
+    for smartieColor, smartieCount in smarties:
         # 13 seconds per group of 7, including non-full group
-        if smartie != "red" and smarties[smartie] != 0:
-            time += (((smarties[smartie] // 7) + 1) * 13)
+        if smartieColor != "red" and smartieCount != 0:
+            time += (((smartieCount // 7) + 1) * 13)
         # 16 seconds per individual red smartie
-        elif smartie == "red":
-            time += (smarties[smartie] * 16)
+        elif smartieColor == "red":
+            time += (smartieCount * 16)
     # Print total time and reset dict + time variables
     print(time)
-    smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
-    time = 0
