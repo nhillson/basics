d135694df8cd6e70289419450bd9854063b63eb3
nhillson <116381076+nhillson@users.noreply.github.com>
2023-06-01 14:48:40 -0500

    Update smarties.py
    Add comments

--- smarties.py
+++ smarties.py
@@ -1,17 +1,23 @@
 # https://dmoj.ca/problem/ecoo15r1p1
 smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
 time = 0
+# 10 test cases
 for _ in range(10):
     while True:
+        # Take input and increment value in smarties dict
         color = input()
         if color == "end of box":
             break
         smarties[color] += 1
+    # Loop through smarties dict accumulating time
     for smartie in smarties:
+        # 13 seconds per group of 7, including non-full group
         if smartie != "red" and smarties[smartie] != 0:
             time += (((smarties[smartie] // 7) + 1) * 13)
+        # 16 seconds per individual red smartie
         elif smartie == "red":
             time += (smarties[smartie] * 16)
+    # Print total time and reset dict + time variables
     print(time)
     smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
     time = 0
