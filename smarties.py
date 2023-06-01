3830f483c7ef329f7029752ec7bd8d2c651e6974
nhillson <116381076+nhillson@users.noreply.github.com>
2023-06-01 15:29:45 -0500

    Update smarties.py

--- smarties.py
+++ smarties.py
@@ -1,24 +1,23 @@
 # https://dmoj.ca/problem/ecoo15r1p1
-# Probably should have initialized inside loop so I wouldn't have to reset variables each iteration
-smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
-time = 0
 # 10 test cases
 for _ in range(10):
-    while True:
+    smarties = {}
+    time = 0
+    color = ""
+    while color != "end of box":
         # Take input and increment value in smarties dict
         color = input()
-        if color == "end of box":
-            break
-        smarties[color] += 1
+        if color in smarties:
+            smarties[color] += 1
+        elif color != "end of box":
+            smarties[color] = 1
     # Loop through smarties dict accumulating time
-    for smartie in smarties:
+    for smartieColor, smartieCount in smarties.items():
         # 13 seconds per group of 7, including non-full group
-        if smartie != "red" and smarties[smartie] != 0:
-            time += (((smarties[smartie] // 7) + 1) * 13)
+        if smartieColor != "red":
+            time += (((smartieCount // 7) + 1) * 13)
         # 16 seconds per individual red smartie
-        elif smartie == "red":
-            time += (smarties[smartie] * 16)
-    # Print total time and reset dict + time variables
+        elif smartieColor == "red":
+            time += (smartieCount * 16)
+    # Print total time
     print(time)
-    smarties = {"orange": 0, "blue": 0, "green": 0, "yellow": 0, "pink": 0, "violet": 0, "brown": 0, "red": 0}
-    time = 0
